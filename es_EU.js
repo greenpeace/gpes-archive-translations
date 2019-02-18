b8b44df25cd6a36b32c3c822b9f636ef5258851b
lucycons3 <info@lucycons3.eu>
2019-02-18 14:48:58 +0100

    Cambios básicos y términos añadidos.

--- es_EU.js
+++ es_EU.js
@@ -1,8 +1,8 @@
 var euskera = [
   //Cabecera
   {
-    "castellano":'Archivo histórico',
-    "euskera":'Artxibo historikoa'
+    "castellano":'ARCHIVO HISTÓRICO',
+    "euskera":'ARTXIBO HISTORIKOA'
   },
   //menu
   {
@@ -72,20 +72,14 @@ var euskera = [
   },
   //OPAC
   {
-    "castellano":"Seleccionar tipo de documento a buscar",
+    "castellano":"\n                    Seleccionar tipo de documento a buscar\n                  ",
     "euskera":"Bilatzeko dokumentu mota aukeratu"
   },{
-    "castellano":"Búsqueda por texto libre",
-    "euskera":"Testu libreko bilaketa"
+    "castellano":'Búsqueda por texto libre<br><span class=\"note\">Solo para documentos a partir de 2004</span>',
+    "euskera":"Testu libreko bilaketa<br><span class=\"note\">2004-tik aurreko dokumentuetarako soilik</span>"
   },{
-    "castellano":"Solo para documentos a partir de 2004.",
-    "euskera":"2004-tik aurreko dokumentuetarako soilik"
-  },{
-    "castellano":"Título del Documento",
-    "euskera":"Dokumentuaren izenburua"
-  },{
-    "castellano":"No utilizar Título para revistas.",
-    "euskera":"Izenbururik ez erabili aldizkarientzat"
+    "castellano":'Título del Documento<br><span class=\"note\">No utilizar Título para revistas</span>',
+    "euskera":"Dokumentuaren izenburua<br><span class=\"note\">Izenbururik ez erabili aldizkarientzat</span>"
   },{
     "castellano":"Seleccionar formato de salida de los resultados",
     "euskera":"Emaitzen irteera formatua aukeratu"
@@ -100,13 +94,56 @@ var euskera = [
     "euskera":'Urtea'
   },{
     "castellano":'Programa',
-    "euskera":'programa'
+    "euskera":'Programa'
   },{
     "castellano":'Época',
     "euskera":'Garaia'
   },{
     "castellano":'Número',
     "euskera":'Zenbakia'
+  },
+  //OPAC Help
+  {
+    'castellano':'Volver a búsqueda',
+    'euskera':''
+  },
+  //OPAC REPORT
+  {
+    'castellano':'Resultados de la búsqueda',
+    'euskera':''
+  },{
+    'castellano':'Número de resultados',
+    'euskera':''
+  },{
+    'castellano':'Autoría Institucional:\n',
+    'euskera':'Egiletza instituzionala:\n'
+  },{
+    'castellano':'Temática:\n',
+    'euskera':'Gaiak:\n'
+  },{
+    'castellano':'Año:\n',
+    'euskera':'Urtea:\n'
+  },{
+    'castellano':'Programa:\n',
+    'euskera':'Programa:\n'
+  },{
+    'castellano':'Época:\n',
+    'euskera':'Garaia:\n'
+  },{
+    'castellano':'Número:\n',
+    'euskera':'Zenbakia:\n'
+  },{
+    'castellano':'Departamento',
+    'euskera':''
+  },{
+    'castellano':'Autoría Individual',
+    'euskera':''
+  },{
+    'castellano':'Departamento',
+    'euskera':''
+  },{
+    'castellano':'Nueva búsqueda',
+    'euskera':''
   },
   //Botones
   {
@@ -122,35 +159,29 @@ var euskera = [
     'castellano':'Salir',
     'euskera':'Irten'
   },{
-    'castellano':'Volver a archivo personal',
+    'castellano':`
+                Volver a archivo personal
+              `,
     'euskera':'Artxibo pertsonalera bueltatu'
   },
   //Pie
   {
-    "castellano":`
-        <p class="left"><a href="https://es.greenpeace.org"><img src="http://localhost:8080/ProdocWeb2/SendDoc?Id=164ec7bc17e-3fccdc9f9ef78520" style="width:110px;height:18px" alt="Greenpeace"></a></p>
-        <p class="center">|</p>
-        <p class="right">Sitio web realizado con <a href="https://jhierrot.github.io/openprodoc/index_ES.html" class="link">OpenProdoc</a>, liberado bajo licencia AGPLv3</p>
-      `,
-    "euskera":`
-        <p class="left"><a href="https://es.greenpeace.org"><img src="http://localhost:8080/ProdocWeb2/SendDoc?Id=164ec7bc17e-3fccdc9f9ef78520" style="width:110px;height:18px" alt="Greenpeace"></a></p>
-        <p class="center">|</p>
-        <p class="right"> <a href="https://jhierrot.github.io/openprodoc/index_ES.html" class="link">OpenProdoc-arekin eginiko web orria</a>, AGPLv3 lizentziapean liberatua</p>
-        `
+    "castellano":`Sitio web realizado con <a href=\"https://jhierrot.github.io/openprodoc/index_ES.html\" class=\"link\">OpenProdoc</a>, liberado bajo licencia AGPLv3`,
+    "euskera":`<a href="https://jhierrot.github.io/openprodoc/index_ES.html" class="link">OpenProdoc</a>-arekin eginiko web orria, AGPLv3 lizentziapean liberatua</p>`
   },
-  //Participa
+  //ContribLogin
   {
     'castellano':'Datos de la persona donante',
     'euskera':'Emaileraen datuak'
   },{
-    "castellano":'Nombre',
-    "euskera":'Izena'
+    "castellano":'Nombre<span style="color:red" ;=""> * </span>',
+    "euskera":'Izena<span style="color:red" ;=""> * </span>'
   },{
     "castellano":'Apellidos',
     "euskera":'Abizenak'
   },{
-    "castellano":'Correo electrónico',
-    "euskera":'Posta elektronikoa'
+    "castellano":'Correo electrónico<span style="color:red" ;=""> * </span>',
+    "euskera":'Posta elektronikoa<span style="color:red" ;=""> * </span>'
   },{
     "castellano":'Comentarios',
     "euskera":'Iruzkinak'
@@ -167,9 +198,20 @@ var euskera = [
     "euskera":`
             <input type="checkbox" required=""> <a href="politica-privacidad.html" target="_blank">Pribatutasun-politika irakurri eta onartu dut</a>.
           `,
+  },{
+    "castellano":`
+                Ayuda
+              `,
+    'euskera':'Laguntza'
   },
   //donaciones
   {
+    'castellano':`Nombre`,
+    'euskera':'Izena'
+  },{
+    'castellano':`Correo electrónico`,
+    'euskera':'Posta elektronikoa'
+  },{
     "castellano":'Añadir nuevo documento',
     'euskera':'Dokumentu berria gehitu'
   },{
@@ -178,11 +220,27 @@ var euskera = [
   },{
     'castellano':'Documentos aportados',
     'euskera':'Emandako dokumentuak'
+  },{
+    'castellano':`
+                    Ayuda
+                `,
+    'euskera':'Laguntza'
+  },{
+    'castellano':`
+                    Salir
+                `,
+    'euskera':'Irten'
   },
   //añadir documento
   {
     'castellano':'Por favor, indícanos el título del documento. Los demás campos son opcionales.',
     'euskera':'Mesedez, dokumentuaren izenburua adierazi. Beste eremuak hautazkoak dira'
+  },{
+    'castellano':'Título del Documento',
+    'euskera':'Dokumentuaren izenburua'
+  },{
+    'castellano':'Descripción del documento',
+    'euskera':''
   },{
     'castellano':'Fecha Documento',
     'euskera':'Dokumentu-data'
@@ -190,11 +248,20 @@ var euskera = [
     'castellano':'Archivo a aportar',
     'euskera':'Emateko artxiboa'
   },{
-    'castellano':'Tamaño máximo: 20 MB',
+    'castellano':'Tamaño máximo: 20 MB.',
     'euskera':'Gehienezko tamaina: 20MB'
+  },{
+    'castellano':'Añadir',
+    'euskera':''
+  },{
+    'castellano':'Volver a donaciones',
+    'euskera':''
   },
   //resultado donacion
   {
+    'castellano':'Archivo personal',
+    'euskera':''
+  },{
     'castellano':'Resultado de la incorporación de documentos',
     'euskera':'Dokumentu-ematearen emaitza'
   },{
@@ -203,5 +270,13 @@ var euskera = [
   },{
     'castellano':'ERROR:Not Allowed extension',
     'euskera':'ERROREA: debekatutako-luzapena'
+  },{
+    'castellano':`
+                  Volver a archivo personal
+              `,
+    'euskera':'Artxibo pertsonalera bueltatu'
+  },{
+    'castellano':'Reintentar',
+    'euskera':''
   }
 ];
